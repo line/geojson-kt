a4ad5db8b0ecfc1f63d509d677b68d908eaf0724
pinzolo <pinzolo@gmail.com>
2025-05-08 12:03:53 +0900

    doc: add doc

--- src/main/kotlin/jp/co/lycorp/geojson/jackson/GoeJsonObjectDeserializer.kt
+++ src/main/kotlin/jp/co/lycorp/geojson/jackson/GoeJsonObjectDeserializer.kt
@@ -15,6 +15,9 @@ import jp.co.lycorp.geojson.MultiPolygon
 import jp.co.lycorp.geojson.Point
 import jp.co.lycorp.geojson.Polygon
 
+/**
+ * Deserializer for all GeoJSON object
+ */
 class GoeJsonObjectDeserializer<T: GeoJsonObject>: JsonDeserializer<T>() {
     override fun deserialize(p: JsonParser, ctxt: DeserializationContext): T {
         val node: JsonNode = p.codec.readTree(p)
