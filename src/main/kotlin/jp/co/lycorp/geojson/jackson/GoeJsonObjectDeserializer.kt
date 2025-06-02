1693a38e52c317a21c38902f8704d3cb92cdecdf
masato-noda <135186339+masato-noda@users.noreply.github.com>
2025-06-02 10:01:44 +0900

    Update src/main/kotlin/jp/co/lycorp/geojson/jackson/GoeJsonObjectDeserializer.kt
    Co-authored-by: robert-mitchell-iii <133644556+robert-mitchell-iii@users.noreply.github.com>

--- src/main/kotlin/jp/co/lycorp/geojson/jackson/GoeJsonObjectDeserializer.kt
+++ src/main/kotlin/jp/co/lycorp/geojson/jackson/GoeJsonObjectDeserializer.kt
@@ -16,7 +16,7 @@ import jp.co.lycorp.geojson.Point
 import jp.co.lycorp.geojson.Polygon
 
 /**
- * Deserializer for all GeoJSON object
+ * Deserializer for all GeoJSON objects.
  */
 class GoeJsonObjectDeserializer<T : GeoJsonObject> : JsonDeserializer<T>() {
     override fun deserialize(p: JsonParser, ctxt: DeserializationContext): T {
